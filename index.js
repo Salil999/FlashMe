29f1819e0de2154edf915fc9ddad7e874cb673d6
Salil999 <ssaxen4@illinois.edu>
2016-02-27 01:44:22 -0500

    added some comments

--- index.js
+++ index.js
@@ -6,31 +6,51 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var errorHandler = require('errorhandler');
 var db = new Firebase("https://spartahack2016.firebaseio.com");
-var users =  db.child('users');
+var users = db.child('users');
+
+//////////////////////
+// INITIAL SETTINGS //
+//////////////////////
+
 app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(path.join(__dirname,'/views')));
-
+app.use(express.static(path.join(__dirname, '/views')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-/*app.use(function(request, response, next) {
-  response.locals.user = request.user;
-  next();
-});*/
-app.set('views', path.join(__dirname,'/views/'));
+app.set('views', path.join(__dirname, '/views/'));
 app.set('view engine', 'jade');
-app.get('/', function(req,res){
-	//res.sendFile(path.join(__dirname, 'views/index.html'));
-	res.render('index');
+
+//////////////////////
+// INITIAL SETTINGS //
+//////////////////////
+
+//////////////////
+// 'GET' ROUTES //
+//////////////////
+
+app.get('/', function(req, res) {
+    //res.sendFile(path.join(__dirname, 'views/index.html'));
+    res.render('index');
 });
-app.get('/signup', function(req,res){
-	res.render('signin');
+
+app.get('/signup', function(req, res) {
+    res.render('signin');
 });
+
+//////////////////
+// 'GET' ROUTES //
+//////////////////
+
+
+
+///////////////////
+// 'POST' ROUTES //
+///////////////////
+
+///////////////////
+// 'POST' ROUTES //
+///////////////////
+
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
-
-exports.app = app;
-
