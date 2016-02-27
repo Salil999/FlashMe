8db8f5d53a4a8c9e9837eba22ddb0a6f21c00e87
Salil999 <ssaxen4@illinois.edu>
2016-02-27 04:07:42 -0500

    updated

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ var db = new Firebase("https://spartahack2016.firebaseio.com");
 var user = require('./refs/user.js');
 var app = express();
 app.set('port', (process.env.PORT || 5000));
-app.set('views', path.join(__dirname,'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.static(path.join(__dirname,'views')));
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,22 +21,21 @@ app.use(cookieParser());
   next();
 });*/
 
-app.get('/signin', function(req,res){
-	//res.render('signin');
-	res.sendFile(path.join(__dirname, 'views/signin.html'));
+app.get('/signin', function(req, res) {
+    //res.render('signin');
+    res.sendFile(path.join(__dirname, 'views/signin.html'));
 });
 app.post('/signin', user.postLogin);
-app.get('/signup', function(req,res){
-	res.sendFile(path.join(__dirname, 'views/signup.html'));
+app.get('/signup', function(req, res) {
+    res.sendFile(path.join(__dirname, 'views/signup.html'));
 });
 app.post('/signup', user.postSignup);
-app.get('/', function(req,res){
-	res.sendFile(path.join(__dirname, 'views/index.html'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
 
 exports.app = app;
-
