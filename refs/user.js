197ede2274521637b6f4f45b7cfff716276ad9cb
Salil999 <ssaxen4@illinois.edu>
2016-02-27 05:31:41 -0500

    done

--- refs/user.js
+++ refs/user.js
@@ -1,24 +1,23 @@
 var db = new Firebase("https://spartahack2016.firebaseio.com");
-var users =  db.child('users');
+var users = db.child('users');
 var tokenGen = require('firebase-token-generator');
 //var token = tokenGen.createToken();
 /**
  * POST /login
  * Sign in using email and password.
  */
-exports.postLogin = function(req,res){
-	db.authWithPassword({
-		email: req.body.email,
-		password: req.body.password
-	}, function(err, authData){
-		if(err){
-			console.log("err!");
-		}
-		else{
-			console.log("auth succes ! ", authData);
-			res.redirect('/');
-		}
-	}, { remember: "sessionOnly"});
+exports.postLogin = function(req, res) {
+    db.authWithPassword({
+        email: req.body.email,
+        password: req.body.password
+    }, function(err, authData) {
+        if (err) {
+            console.log("err!");
+        } else {
+            console.log("auth succes ! ", authData);
+            res.redirect('/');
+        }
+    }, { remember: "sessionOnly" });
 };
 /**
  * POST /signup
@@ -26,42 +25,40 @@ exports.postLogin = function(req,res){
  */
 exports.postSignup = function(req, res) {
 
-	db.createUser({
-		email: req.body.email,
-		password: req.body.password
-	}, function(error, authData){
-		if(error){
-			console.log("err: ", error);
-		}
-		else {
-			console.log("success! with uid: ", authData.uid);
-			/* after the user is signed, log him/her in (Firebase DOES NOT do this for you) */
-			db.authWithPassword({
-				email: req.body.email,
-				password: req.body.password
-			}, function(err, authData){
-				if(err){
-					console.log("err! " + err);
-				}
-				else{
-					console.log("auth succes ! ", authData);
-					res.redirect('/');
-				}
-			}, { remember: "sessionOnly" });
-			db.onAuth(function(data){
-				users.child(authData.uid).set({
-					name:toTitleCase(req.body.name)
-				});
-			});
-		}
-	});
+    db.createUser({
+        email: req.body.email,
+        password: req.body.password
+    }, function(error, authData) {
+        if (error) {
+            console.log("err: ", error);
+        } else {
+            console.log("success! with uid: ", authData.uid);
+            /* after the user is signed, log him/her in (Firebase DOES NOT do this for you) */
+            db.authWithPassword({
+                email: req.body.email,
+                password: req.body.password
+            }, function(err, authData) {
+                if (err) {
+                    console.log("err! " + err);
+                } else {
+                    console.log("auth succes ! ", authData);
+                    res.redirect('/');
+                }
+            }, { remember: "sessionOnly" });
+            db.onAuth(function(data) {
+                users.child(authData.uid).set({
+                    name: toTitleCase(req.body.name)
+                });
+            });
+        }
+    });
 
 };
 
-var toTitleCase = function(str){
-	return str.replace(/\w\S*/g, function(txt) {
-		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-	});
+var toTitleCase = function(str) {
+    return str.replace(/\w\S*/g, function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
 };
 /**
  * GET /logout
@@ -76,7 +73,7 @@ var toTitleCase = function(str){
  * GET /signup
  * Signup page.
  */
- /*
+/*
 exports.getSignup = function(request, response) {
   //if (req.user) return res.redirect('/');
   response.render('pages/signup');
@@ -87,7 +84,7 @@ exports.getSignup = function(request, response) {
  * POST /signup
  * Create a new local account.
  */
- /*
+/*
 exports.postSignup = function(req, res, next) {
 
 User.findOne({where: {username:req.body.username}})
